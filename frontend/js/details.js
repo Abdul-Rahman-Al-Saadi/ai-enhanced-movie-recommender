15fa29732059a52ad029ce627f4322069fd50db0
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-11-10 07:51:51 +0200

    Added form popup, appears if not logged in

--- frontend/js/details.js
+++ frontend/js/details.js
@@ -1,14 +1,34 @@
+let isLoggedIn = false;
+
+
 // toggeling the visibility of bookmark
 function toggleVisibility(element){
-    element.style.display = (element.style.display === "none") ? "block" : "none";
+    element.style.display = (element.style.display === "none" || element.style.display === "" ) ? "block" : "none";
 }
+function openForm(){
+    mainElement.classList.add('blurred');
+    document.getElementById('myForm').style.display = 'block';
+}
+function closeForm(){
+    document.getElementById('myForm').style.display = 'none';
+}
+
+const formPopup = document.getElementById('myForm');
+const mainElement = document.querySelector('main');
+
+const cancelBtn = document.querySelector('.cancel');
+cancelBtn.addEventListener('click', () =>{
+    mainElement.classList.remove('blurred');
+    toggleVisibility(formPopup);
+})
 
 const bookmarkIcon = document.getElementById('bookmark-icon');
 document.getElementById('bookmark-btn').addEventListener('click', () =>{
-    toggleVisibility(bookmarkIcon);
+    isLoggedIn ? toggleVisibility(bookmarkIcon) : openForm();
 })
 
-document.addEventListener('DOMContentLoaded', function() {
+// styling and saving the star rating
+document.addEventListener('DOMContentLoaded', () => {
     const stars = document.querySelectorAll('.star');
     const submitButton = document.getElementById('submit-rating');
     let selectedRating = 0;
@@ -44,20 +64,15 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
-
     function resetStars() {
         stars.forEach(star => {
             star.classList.remove('selected');
         });
     }
-
     function updateRatingDisplay() {
         ratingValueElement.textContent = selectedRating;
         highlightStars(selectedRating);
     }
-
-    submitButton.addEventListener('click', function() {
-        alert(`You rated this movie ${selectedRating} stars!`);
-    });
 });
+
 
