00406d97d680c5056d4a7f8d5bcdf7d9b93743c4
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-11-11 23:17:37 +0200

    Created getUserActivities api

---
+++ server/getUserActivities.php
@@ -0,0 +1,44 @@
+<?php
+
+include "connection.php";
+
+if($connection->connect_error){
+    die("Unsuccessful connection with the Database");
+}
+
+if(isset($_POST['user_id'])){
+    $user_id = $_POST['user_id'];
+}else{
+    die("Unvalid Body Parameters");
+}
+
+$queryStatment = "SELECT m.movie_name
+FROM movies_db.movies m
+JOIN movies_db.ratings r ON m.movie_id = r.movie_id
+WHERE r.user_id = ? AND r.rating > 3
+UNION
+SELECT m.movie_name
+FROM movies_db.movies m
+JOIN movies_db.bookmarks b ON m.movie_id = b.movie_id
+WHERE b.user_id = ?
+UNION
+SELECT m.movie_name
+FROM movies_db.movies m
+JOIN movies_db.user_activities a ON m.movie_id = a.movie_id
+WHERE a.user_id = ?;";
+
+$query = $connection->prepare($queryStatment);
+$query->bind_param('iii', $user_id, $user_id, $user_id);
+$query->execute();
+$result = $query->get_result();
+
+if($result->num_rows > 0){
+    $array = [];
+
+    while($resultObject = $result->fetch_assoc()){
+        $array [] = $resultObject;
+    }
+    echo json_encode($array);
+}else{
+    echo json_encode(["message"=> "No movies Found"]);
+}
