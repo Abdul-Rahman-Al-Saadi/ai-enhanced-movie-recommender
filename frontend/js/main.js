9a88dd7d1db036ef9b77ae3e869c16f02d301fa6
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-11-14 07:09:24 +0200

    Created a list of objects having only id and title

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,21 +1,33 @@
 
 // For fetching the movies
-    // const fetchMovies = async () => {
-    //     try {
-    //         const response = await fetch("http://localhost/ai-enhanced-movie-recommender/server/getMovies.php");
-    //         if (response.ok) {
-    //             console.log("successful");
-    //             const data = await response.json();
-    //             return data;
-    //         } else {
-    //             console.log("unsuccessful");
-    //             return [];
-    //         }
-    //     } catch (error) {
-    //         console.error("Error fetching movies:", error);
-    //         return [];
-    //     }
-    // };
+    const fetchMovies = async () => {
+        try {
+            const response = await fetch("http://localhost/ai-enhanced-movie-recommender/server/getMovies.php");
+            if (response.ok) {
+                console.log("successful");
+                const data = await response.json();
+                getIdAndTitle(data);
+                // return data;
+            } else {
+                console.log("unsuccessful");
+                return [];
+            }
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+            return [];
+        }
+    };
+
+    function getIdAndTitle(data){
+        const idMovieList = data.map(item => ({
+            id: item.movie_id,
+            title: item.title,
+        }))
+        // console.log(subList);
+        console.log(idMovieList);
+    }
+
+    fetchMovies();
 
 
 // Inside the dynamic creation of movies
@@ -30,21 +42,6 @@
     // let user_id = 3; 
     // localStorage.setItem('user_id', user_id);
 
-    // import OpenAI from "openai";
-    // const openai = new OpenAI();
-
-    // // openai.api_key = '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';
-    // const openAIClient = new OpenAI({
-    //     apiKey: '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'
-    // })
-
-    // const completion = await openAIClient.chat.completion.create({
-    //     model: 'gpt-3.5-turbo',
-        // messages:[
-        //     {role:"user", content:"hello there"}
-        // ],
-    // })
-    // console.log(completion);
 
     async function callOpenAI() {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
